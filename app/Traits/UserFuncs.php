ce8c999f19a331c6894400905f4609033db43582
2000jester <2000jester@gmail.com>
2017-11-16 08:56:13 +1100

    generic push

---
+++ app/Traits/UserFuncs.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Traits;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\DB;
+use App\users;
+use Cookie;
+
+trait UserFuncs{
+}
