ae6453e55dc4d047f32ad2f5878e3e1422d952e2
2000jester <2000jester@gmail.com>
2017-12-06 10:52:53 +1100

    generic push

--- resources/views/pages/dupes.blade.php
+++ resources/views/pages/dupes.blade.php
@@ -4,9 +4,11 @@
 @endsection
 @section('body')
     <div class="full-height content flex-center">
+    <div class="main-content">
         @for($i = 0; $i < count($dupes); $i++)
             {{ $dupes[$i]->title." : ".$dupes[$i]->barcode }}</br>
         @endfor
+    </div>
         <div class="fixed-button">
             <form action="/">
                 <input type="submit" value="Menu" class="button">
